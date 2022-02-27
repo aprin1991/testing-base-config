a1a808cc13655e196b080af8bd4d93a6a9a2f3dd
aprin1991 <hamed@mystage.ca>
2022-02-27 18:05:04 +0330

    init

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  "globals": {
+    "ts-jest": {
+      tsconfig: "tsconfig.jest.json"
+    }
+  },
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+  "coverageThreshold": {
+    "global": {
+      "branches": 100,
+      "functions": 100,
+      "lines": 100,
+      "statements": 100
+    }
+  }
+};
